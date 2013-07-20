4a8e73e064373907f3a2674ede1aed671063a0ca
Umut Kanbak <uk@kanbak.net>
2013-07-20 15:01:15 -0500

    Version 1 (minus Timer)
    did not implement the timer

--- Views/ViewController.m
+++ Views/ViewController.m
@@ -10,16 +10,23 @@
 #import "MyView.h"
 @interface ViewController ()
 {
-    int i;
+    
     int card1;
     int card2;
-    bool secondCard;
+    BOOL touch1;
+    BOOL touch2;
     MyView *firstCard;
     MyView *lastCard;
-    bool isItAWin;
+    BOOL isItAWin;
     int timesWon;
-    NSMutableArray *wins;
+    __weak IBOutlet UILabel *matchedMissedCountLabel;
+    __weak IBOutlet UIButton *resetGameButton;
+    __weak IBOutlet UIImageView *missedOrMatchedImage;
+    __weak IBOutlet UIImage *missedImage;
+    __weak IBOutlet UIImage *matchedImage;
+    
 }
+- (IBAction)resetGame:(id)sender;
 
 @end
 
@@ -35,17 +42,21 @@
             MyView * myView = (MyView *)subView;
             myView.delegate = self;
             timesWon = 0;
-                    }
+            matchedMissedCountLabel.text=@"Matching Game";
+            [missedOrMatchedImage setImage:nil];
+            missedImage = [UIImage imageNamed:@"missed.png"];
+            matchedImage = [UIImage imageNamed:@"matched.png"];
+            
+        }
         
         {
-            wins = [[NSMutableArray alloc]initWithCapacity:8];
-            if (isItAWin == YES) {
-                
-            }
+            
+            
         }
     }
-    
 }
+
+
 
 
 
@@ -57,35 +68,68 @@
 -(void)didChooseView:(MyView*)myView
 {
     
-    if (secondCard == NO) {
+    
+    if (touch1 == NO)
+    {
         card1 = myView.tag;
         firstCard = myView;
-        secondCard = YES;
-        if (timesWon == 8) {
-            timesWon=0;
-
-        }
-        
-    } else {
+        touch1 = YES;
+        touch2= NO;
+    }
+    
+    else if(touch1 == YES)
+    {
         card2 = myView.tag;
         lastCard = myView;
-        secondCard = NO;
-            
-        if (firstCard.tag == lastCard.tag) {
-            NSLog(@"You Win!");
-            isItAWin = YES;
-            timesWon++;
-            NSLog(@"times won = %i", timesWon);
-            firstCard.backgroundColor = [UIColor lightGrayColor];
-            lastCard.backgroundColor = [UIColor lightGrayColor];
-        }else {
-                NSLog(@"You Lose!");
-                isItAWin = NO;}
-        
+        touch2 = YES;
+        touch1=NO;
+    }
+    
+    if ((card1 == card2) && (touch2==YES))
+    {
+        matchedMissedCountLabel.text=@"Match!";
+        [missedOrMatchedImage setImage:matchedImage];
+        NSLog(@"You Win!");
+        isItAWin = YES;
+        timesWon++;
+        NSLog(@"times won = %i", timesWon);
+        firstCard.backgroundColor = [UIColor lightGrayColor];
+        lastCard.backgroundColor = [UIColor lightGrayColor];
+        touch1=NO;
+        touch2=NO;
+    }
+    
+    if ((card1!=card2)&&(touch2==YES))
+    {
+        matchedMissedCountLabel.text=@"Missed!";
+        NSLog(@"You Lose!");
+        isItAWin = NO;
+        [missedOrMatchedImage setImage:missedImage];
+        missedOrMatchedImage.backgroundColor = [UIColor lightGrayColor];
+    }
+    
+    if (timesWon == 8)
+    {
+        for (MyView *subview in self.view.subviews)
+            subview.backgroundColor = [UIColor brownColor];
+        matchedMissedCountLabel.backgroundColor = [UIColor lightGrayColor];
+        missedOrMatchedImage.backgroundColor = [UIColor lightGrayColor];
+        timesWon=0;
+    }
+    
+    
 }
-        }
-            
 
 
-
+- (IBAction)resetGame:(id)sender {
+    for (MyView *subview in self.view.subviews)
+        subview.backgroundColor = [UIColor brownColor];
+    matchedMissedCountLabel.backgroundColor = [UIColor lightGrayColor];
+    resetGameButton.backgroundColor=[UIColor lightGrayColor];
+    timesWon=0;
+    matchedMissedCountLabel.text=@"Matching Game";
+    [missedOrMatchedImage setImage:matchedImage];
+    missedOrMatchedImage.backgroundColor = [UIColor lightGrayColor];
+    
+}
 @end
